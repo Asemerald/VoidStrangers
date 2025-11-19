9dbae0972aa4c9e8939d0af2ddb13a3ed336c9f7
LeBud <c.huyghe@rubika-edu.com>
2025-11-19 17:10:44 +0100

    Tiles

--- Assets/00_Scripts/LevelSetup.cs
+++ Assets/00_Scripts/LevelSetup.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using _00_Scripts;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -6,14 +8,47 @@ public class LevelSetup : MonoBehaviour {
     public static LevelSetup Instance { get; private set; }
     
     [SerializeField] private Tilemap tileMap;
+    [SerializeField] private List<TilesData> tilesData;
+    
+    private Dictionary<TileBase, TilesData> dataFromTiles =  new Dictionary<TileBase, TilesData>();
     
     private void Start() {
         if(Instance == null) Instance = this;
         else Destroy(this);
+
+        foreach (var tileData in tilesData) {
+            foreach (var tile in tileData.tiles) {
+                dataFromTiles.Add(tile, tileData);
+            }
+        }
     }
 
-    public void GetTile(Vector2Int position) {
-        Debug.Log(tileMap.GetTile(new Vector3Int(position.x - 1, position.y - 1, 0)));
+    private void GetTile(Vector2Int position) {
+        Debug.Log(tileMap.GetTile(new Vector3Int(position.x, position.y, 0)));
+    }
+
+    public bool CanMove(Vector3 position) {
+        var fx = Mathf.FloorToInt(position.x);
+        var fy = Mathf.FloorToInt(position.y);
+        var fz = Mathf.FloorToInt(position.z);
+        
+        var cx = Mathf.CeilToInt(position.x);
+        var cy = Mathf.CeilToInt(position.y);
+        var cz = Mathf.CeilToInt(position.z);
+
+        if (dataFromTiles[tileMap.GetTile(new Vector3Int(fx, fy, fz))].walkable) {
+            if(!dataFromTiles[tileMap.GetTile(new Vector3Int(cx, cy, cz))].walkable) return false;
+            
+            return true;
+        }
+        
+        if (dataFromTiles[tileMap.GetTile(new Vector3Int(cx, cy, cz))].walkable) {
+            if(!dataFromTiles[tileMap.GetTile(new Vector3Int(fx, fy, fz))].walkable) return false;
+            
+            return true;
+        }
+        
+        return false;
     }
     
 }
